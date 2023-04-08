a67eb7a4cdda9abce7e2acbb47bcfa778fce2557
Ali <ali.mirghasemi1376@gmail.com>
2023-04-08 15:35:57 +0430

    Add pointer array helper

--- Src/Serde.c
+++ Src/Serde.c
@@ -60,12 +60,28 @@ static void* Serde_Array_moveAddress(void* pValue, Serde_Type type, Serde_LenTyp
     return (uint8_t*) pValue + objSize;
 }
 
+static void* Serde_PointerArray_alignAddress(void* pValue, Serde_Type type, Serde_LenType len, Serde_LenType len2) {
+    uint8_t objSize = sizeof(void*);
+    uint32_t pobj = (uint8_t*) pValue;
+
+    if (pobj % objSize != 0) {
+        pobj += objSize - pobj % objSize;
+    }
+
+    return (void*) pobj;
+}
+static void* Serde_PointerArray_moveAddress(void* pValue, Serde_Type type, Serde_LenType len, Serde_LenType len2) {
+    uint8_t objSize = sizeof(void*) * len;
+    return (uint8_t*) pValue + objSize;
+}
+
 #define SERDE_HELPER(CAT)           { Serde_ ##CAT ##_alignAddress, Serde_ ##CAT ##_moveAddress }
 
 static const Serde_Type_Helper SERDE_HELPER[Serde_Category_Length] = {
     SERDE_HELPER(Primary),
     SERDE_HELPER(Pointer),
     SERDE_HELPER(Array),
+    SERDE_HELPER(PointerArray),
 };
 
 void Serde_serializePrimary(void* out, const uint8_t* fmt, void* obj, Serde_SerializeFn* serialize) {
